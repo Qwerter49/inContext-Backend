fa1334ab5d255d53fe6b476644e832ea901c3822
Jay <jaykamas@gmail.com>
2020-11-05 15:34:31 -0500

    Changed logic to fit heroku tutorial

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const express = require("express")
-const app = express()
-const http = require("http")
-const socketIO = require("socket.io")
-const httpApp = http.createServer(app)
-const io = socketIO(httpApp)
+const express = require('express');
+const socketIO = require('socket.io');
 
-io.on("connection", socket => {
-    console.log("Someone connected!")
-    socket.on("message", message => {
-        console.log("Someone sent a message!", message)
-    })
-})
 
-const PORT = process.env.PORT || 4000
-httpApp.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
-})
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+    .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const io = socketIO(server);
+
+
+io.on('connection', (socket) => {
+    console.log('Client connected');
+    socket.on('disconnect', () => console.log('Client disconnected'));
+});
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
